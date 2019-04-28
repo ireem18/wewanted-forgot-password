e3d91a4d4fd361a308f643d703ef95cf4aac563f
İrem Demiröz <40467163+ireem18@users.noreply.github.com>
2019-04-29 00:07:22 +0300

    Update Right.js

--- src/components/Right.js
+++ src/components/Right.js
@@ -56,34 +56,4 @@ export default function Right() {
   }
 
 
-  /*
-  <div className="form-container sign-in-container FrameLogin ">
-  <img class="Logo1" src={logo} style={{size:'100px x 10px'}}alt="logo" />
-    <p className="Lorem-Ipsum-is-simply-dummy-text-of-the-printing-industry">
-    Lorem Ipsum is simply dummy text of the printing industry.
-    </p>
-    <p className="Lorem-Ipsum-has-been-the-industrys-standard-dummy-text-ever-since-the-1500s-when-an-unknown-printer-took-a-galley-of-type-it-specimen-book">
-      Lorem ipsum has been the industry standerd dummy text ever since the 1500s,when an unknown printer took a gallery
-      of the type it specimen book.
-    </p>
-    <div className="Inputs">
-
-    <input type="text " className="Input-Base" placeholder='E-posta adresi'/>
-    <button type="button" className="baseButton">Sıfırlama Maili Gönder</button>
-    </div>
-      <div className="footer" style={{ marginTop: '45px', marginRight: '30px</div>' }}>
-        <span className="Giris-yapa-don">Giriş Yap'a Geri Dön</span>
-        <hr className="hr" style={{ backgroundColor:'#f89f23'}}/>
-      </div>
-
-      <div className="footer" style={{marginTop:'90px',marginRight:'50px'}}>
-        <span className="Aramza-katlmak-ister-misin-Kayt-Ol">
-          Aramıza katılmak ister misin?{' '}
-        </span>
-        <span  className="kayit-ol" id="signUp">
-
-          Kayıt Ol
-        </span>
-      </div>
-
-      */
+ 
